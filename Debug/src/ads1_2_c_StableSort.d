2ea982627833d84fcf00b46b612b5124203a6be1
modeverv <modeverv@gmail.com>
2019-12-10 23:48:29 +0900

    a

---
+++ Debug/src/ads1_2_c_StableSort.d
@@ -0,0 +1,1 @@
+src/ads1_2_c_StableSort.o: ../src/ads1_2_c_StableSort.cpp
---
+++ src/ads1_2_c_StableSort.cpp
@@ -0,0 +1,80 @@
+//============================================================================
+// Name        : ads1_2_c_StableSort.cpp
+// Author      : lovesaemi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+struct Card {
+	char suit, value;
+};
+
+void bubble(struct Card A[], int N) {
+	for (int i = 0; i < N; i++) {
+		for (int j = N - 1; j >= i + 1; j--) {
+			if (A[j].value < A[j - 1].value) {
+				Card t = A[j];
+				A[j] = A[j - 1];
+				A[j - 1] = t;
+			}
+		}
+	}
+}
+
+void selection(struct Card A[], int N) {
+	for (int i = 0; i < N; i++) {
+		int minj = i;
+		for (int j = i; j < N; j++) {
+			if (A[j].value < A[minj].value)
+				minj = j;
+		}
+		Card t = A[i];
+		A[i] = A[minj];
+		A[minj] = t;
+	}
+}
+
+void print(struct Card A[], int N) {
+	for (int i = 0; i < N; i++) {
+		if (i > 0)
+			cout << " ";
+		cout << A[i].suit << A[i].value;
+	}
+	cout << endl;
+}
+
+bool isStable(struct Card c1[], struct Card c2[], int N) {
+	for (int i = 0; i < N; i++) {
+		if (c1[i].suit != c2[i].suit) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	Card c1[100], c2[100];
+	int N;
+	char ch;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> c1[i].suit >> c1[i].value;
+	}
+	for (int i = 0; i < N; i++) {
+		c2[i] = c1[i];
+	}
+	bubble(c1, N);
+	selection(c2, N);
+	print(c1, N);
+	cout << "Stable" << endl;
+	print(c2, N);
+	if (isStable(c1, c2, N)) {
+		cout << "Stable" << endl;
+	} else {
+		cout << "Not stable" << endl;
+	}
+}
